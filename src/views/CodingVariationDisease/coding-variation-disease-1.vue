7577eb4eed1d63d6e1be1d0fa1dc02114eb78f9b
Lovelumine <shendekoudai@lovelumine.com>
2024-05-17 21:23:57 +0800

    修改coding-variation-disease页面，并调整为英文

--- src/views/CodingVariationDisease/coding-variation-disease-1.vue
+++ src/views/CodingVariationDisease/coding-variation-disease-1.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="site--main">
-    <h2>Coding variation Disease</h2>
+    <h2>Coding Variation Disease</h2>
     <!-- 顶部行包含尺寸调整和搜索框 -->
     <div class="top-controls">
       <!-- 搜索框 -->
       <div class="search-box" style="margin-bottom: 10px">
-        <input v-model="searchText" placeholder="输入搜索内容" class="search-input">
+        <input v-model="searchText" placeholder="Enter search content" class="search-input">
       </div>
       <!-- 调整尺寸 -->
       <div class="size-controls" style="margin-bottom: 10px">
         <el-radio-group v-model="tableSize">
-          <el-radio-button label="small">小尺寸</el-radio-button>
-          <el-radio-button label="default">默认尺寸</el-radio-button>
-          <el-radio-button label="large">大尺寸</el-radio-button>
+          <el-radio-button value="small">Small Size</el-radio-button>
+          <el-radio-button value="default">Default Size</el-radio-button>
+          <el-radio-button value="large">Large Size</el-radio-button>
         </el-radio-group>
       </div>
     </div>
@@ -22,37 +22,29 @@
         :columns="columns"
         :data-source="filteredDataSource"
         :row-key="record => record.key"
-        :pagination="pagination"
         :stripe="true"
         :show-sorter-tooltip="true"
         :size="tableSize"
-        @sorter-change="onSorterChange"
-        @resize-column="onResizeColumn"
-        @pagination-change="onPaginationChange"
-        :row-expandable="rowExpandable"
-        :expand-icon-column-index="expandIconColumnIndex"
-        :expand-row-by-click="expandRowByClick"
-        @expand="onExpand"
-        @expandedRowsChange="onExpandedRowsChange"
+        :expand-row-by-click="true"
       >
         <template #expandedRowRender="{ record }">
           <div>
-            <p><b>突变类型:</b> {{ record.mutationType }}</p>
-            <p><b>疾病名称:</b> {{ record.diseaseName }}</p>
-            <p><b>Phenotype MIM number:</b> {{ record.Phenotype }}</p>
-            <p><b>致病基因:</b> {{ record.gene }}</p>
-            <p><b>Gene/Locus MIM number:</b> {{ record.Locus }}</p>
-            <p><b>变异位点:</b> {{ record.mutationSite }}</p>
-            <p><b>原有密码子及氨基酸:</b> {{ record.originalCodon }}</p>
-            <p><b>突变后密码子及氨基酸:</b> {{ record.mutatedCodon }}</p>
-            <p><b>染色体:</b> {{ record.chromosome }}</p>
-            <p><b>Genome position:</b> {{ record.Genomeposition }}</p>
-            <p><b>de novo / inherited:</b> {{ record.denovoinherited }}</p>
-            <p><b>发病率:</b> {{ record.incidenceRate }}</p>
-            <p><b>zygosity:</b> {{ record.zygosity }}</p>
-            <p><b>诊断/治疗方案:</b> {{ record.treatmentPlan }}</p>
-            <p><b>参考文献:</b> <a :href="record.References" target="_blank" class="tilt-hover">参考文献</a></p>
-            <p><b>来源:</b> <a :href="record.source" target="_blank" class="tilt-hover">链接</a></p>
+            <p><b>Mutation Type:</b> {{ record.mutationType }}</p>
+            <p><b>Disease Name:</b> {{ record.diseaseName }}</p>
+            <p><b>Phenotype MIM Number:</b> {{ record.Phenotype }}</p>
+            <p><b>Gene:</b> {{ record.gene }}</p>
+            <p><b>Gene/Locus MIM Number:</b> {{ record.Locus }}</p>
+            <p><b>Mutation Site:</b> {{ record.mutationSite }}</p>
+            <p><b>Original Codon and Amino Acid:</b> {{ record.originalCodon }}</p>
+            <p><b>Mutated Codon and Amino Acid:</b> {{ record.mutatedCodon }}</p>
+            <p><b>Chromosome:</b> {{ record.chromosome }}</p>
+            <p><b>Genome Position:</b> {{ record.Genomeposition }}</p>
+            <p><b>De Novo / Inherited:</b> {{ record.denovoinherited }}</p>
+            <p><b>Incidence Rate:</b> {{ record.incidenceRate }}</p>
+            <p><b>Zygosity:</b> {{ record.zygosity }}</p>
+            <p><b>Treatment Plan:</b> {{ record.treatmentPlan }}</p>
+            <p><b>References:</b> <a :href="record.References" target="_blank" class="tilt-hover">References</a></p>
+            <p><b>Source:</b> <a :href="record.source" target="_blank" class="tilt-hover">Link</a></p>
           </div>
         </template>
       </s-table>
@@ -63,12 +55,11 @@
 <script lang="tsx">
 import { defineComponent, ref, onMounted } from 'vue';
 import { STableProvider } from '@shene/table';
-import type { STableColumnsType } from '@shene/table'; 
+import type { STableColumnsType } from '@shene/table';
 import { useTableData } from '../../assets/js/useTableData.js';
 
 // 定义数据类型
 type DataType = { [key: string]: string };
-
 
 export default defineComponent({
   name: 'CodingVariationDisease',
@@ -82,9 +73,9 @@ export default defineComponent({
 
     const columns: STableColumnsType<DataType> = [
       {
-        title: '突变类型',
+        title: 'Mutation Type',
         dataIndex: 'mutationType',
-        width: 120, ellipsis: true,
+        width: 140, ellipsis: true,
         key: 'mutationType',
         resizable: true,
         filter: {
@@ -96,17 +87,17 @@ export default defineComponent({
           onFilter: (value, record) => value.includes(record.mutationType)
         }
       },
-      { title: '疾病名称', dataIndex: 'diseaseName', width: 320, ellipsis: true, key: 'diseaseName', resizable: true },
-      { title: 'Phenotype MIM number', dataIndex: 'Phenotype', width: 200, ellipsis: true, key: 'Phenotype', resizable: true },
-      { title: '致病基因', dataIndex: 'gene', width: 120, ellipsis: true, key: 'gene', resizable: true },
-      { title: 'Gene/Locus MIM number', dataIndex: 'Locus', width: 200, ellipsis: true, key: 'Locus', resizable: true },
-      { title: '变异位点', dataIndex: 'mutationSite', width: 120, ellipsis: true, key: 'mutationSite', resizable: true },
-      { title: '原有密码子及氨基酸', dataIndex: 'originalCodon', width: 140, ellipsis: true, key: 'originalCodon', resizable: true },
-      { title: '突变后密码子及氨基酸', dataIndex: 'mutatedCodon', width: 180, ellipsis: true, key: 'mutatedCodon', resizable: true },
-      { title: '染色体', dataIndex: 'chromosome', width: 120, ellipsis: true, key: 'chromosome', resizable: true },
-      { title: 'Genome position', dataIndex: 'Genomeposition', width: 220, ellipsis: true, key: 'Genomeposition', resizable: true },
+      { title: 'Disease Name', dataIndex: 'diseaseName', width: 320, ellipsis: true, key: 'diseaseName', resizable: true },
+      { title: 'Phenotype MIM Number', dataIndex: 'Phenotype', width: 200, ellipsis: true, key: 'Phenotype', resizable: true },
+      { title: 'Gene', dataIndex: 'gene', width: 120, ellipsis: true, key: 'gene', resizable: true },
+      { title: 'Gene/Locus MIM Number', dataIndex: 'Locus', width: 200, ellipsis: true, key: 'Locus', resizable: true },
+      { title: 'Mutation Site', dataIndex: 'mutationSite', width: 120, ellipsis: true, key: 'mutationSite', resizable: true },
+      { title: 'Original Codon and Amino Acid', dataIndex: 'originalCodon', width: 240, ellipsis: true, key: 'originalCodon', resizable: true },
+      { title: 'Mutated Codon and Amino Acid', dataIndex: 'mutatedCodon', width: 240, ellipsis: true, key: 'mutatedCodon', resizable: true },
+      { title: 'Chromosome', dataIndex: 'chromosome', width: 120, ellipsis: true, key: 'chromosome', resizable: true },
+      { title: 'Genome Position', dataIndex: 'Genomeposition', width: 220, ellipsis: true, key: 'Genomeposition', resizable: true },
       {
-        title: 'de novo / inherited',
+        title: 'De Novo / Inherited',
         dataIndex: 'denovoinherited',
         width: 180, ellipsis: true,
         key: 'denovoinherited',
@@ -121,23 +112,23 @@ export default defineComponent({
         }
       },
       {
-        title: '发病率',
+        title: 'Incidence Rate',
         dataIndex: 'incidenceRate',
         width: 320, ellipsis: true,
         key: 'incidenceRate',
         resizable: true,
         sorter: (a, b) => parseFloat(a.incidenceRate) - parseFloat(b.incidenceRate)
       },
-      { title: 'zygosity', dataIndex: 'zygosity', width: 140, ellipsis: true, key: 'zygosity', resizable: true },
-      { title: '诊断/治疗方案', dataIndex: 'treatmentPlan', width: 320, ellipsis: true, key: 'treatmentPlan', resizable: true },
+      { title: 'Zygosity', dataIndex: 'zygosity', width: 140, ellipsis: true, key: 'zygosity', resizable: true },
+      { title: 'Treatment Plan', dataIndex: 'treatmentPlan', width: 320, ellipsis: true, key: 'treatmentPlan', resizable: true },
       {
-        title: '参考文献', width: 120, ellipsis: true, key: 'References', dataIndex: 'References',
-        customRender: ({ text, record }) => (<div><a href={text || '#'} target="_blank" class="bracket-links">来源</a></div>),
+        title: 'References', width: 120, ellipsis: true, key: 'References', dataIndex: 'References',
+        customRender: ({ text, record }) => (<div><a href={text || '#'} target="_blank" class="bracket-links">References</a></div>),
         resizable: true
       },
       {
-        title: '来源', width: 120, ellipsis: true, key: 'source', dataIndex: 'source',
-        customRender: ({ text, record }) => (<div><a href={text || '#'} target="_blank" class="bracket-links">来源</a></div>),
+        title: 'Source', width: 120, ellipsis: true, key: 'source', dataIndex: 'source',
+        customRender: ({ text, record }) => (<div><a href={text || '#'} target="_blank" class="bracket-links">Link</a></div>),
         resizable: true
       }
     ];
@@ -151,5 +142,3 @@ export default defineComponent({
   }
 });
 </script>
-
-
--- src/views/CodingVariationDisease/coding-variation-disease-2.vue
+++ src/views/CodingVariationDisease/coding-variation-disease-2.vue
@@ -1,102 +1,154 @@
 <template>
-    <div class="site--main">
-      <h2>Coding variation in Cancer</h2>
-      <!-- 顶部行包含尺寸调整和搜索框 -->
-      <div class="top-controls">
-        <!-- 搜索框 -->
-        <div class="search-box" style="margin-bottom: 10px">
-          <input v-model="searchText" placeholder="输入搜索内容" class="search-input">
-        </div>
-        <!-- 调整尺寸 -->
-        <div class="size-controls" style="margin-bottom: 10px">
-          <el-radio-group v-model="tableSize">
-            <el-radio-button label="small">小尺寸</el-radio-button>
-            <el-radio-button label="default">默认尺寸</el-radio-button>
-            <el-radio-button label="large">大尺寸</el-radio-button>
-          </el-radio-group>
-        </div>
+  <div class="site--main">
+    <h2>Coding Variation in Cancer</h2>
+    <!-- 顶部行包含尺寸调整和搜索框 -->
+    <div class="top-controls">
+      <!-- 搜索框 -->
+      <div class="search-box" style="margin-bottom: 10px">
+        <input v-model="searchText" placeholder="Enter search content" class="search-input">
       </div>
-      <!-- 表格组件 -->
-      <s-table-provider :hover="true" :theme-color="'#00ACF5'">
-        <s-table
-          :columns="columns"
-          :data-source="filteredDataSource"
-          :row-key="record => record.key"
-          :pagination="pagination"
-          :stripe="true"
-          :show-sorter-tooltip="true"
-          :size="tableSize"
-          @sorter-change="onSorterChange"
-          @resize-column="onResizeColumn"
-          @pagination-change="onPaginationChange"
-          :row-expandable="rowExpandable"
-          :expand-icon-column-index="expandIconColumnIndex"
-          :expand-row-by-click="expandRowByClick"
-          @expand="onExpand"
-          @expandedRowsChange="onExpandedRowsChange"
-        >
-          <template #expandedRowRender="{ record }">
-            <div>
-              <p><b>GENE NAME:</b> {{ record.GENE_NAME }}</p>
-              <p><b>ENSEMBL ID:</b> {{ record.ENSEMBL_ID }}</p>
-              <p><b>MUTATION URL:</b> <a :href="record.MUTATION_URL" target="_blank">{{ record.MUTATION_URL }}</a></p>
-              <p><b>LEGACY MUTATION ID:</b> {{ record.LEGACY_MUTATION_ID }}</p>
-              <p><b>GENOMIC MUTATION ID:</b> {{ record.GENOMIC_MUTATION_ID }}</p>
-              <p><b>MUTATION LOCUS IN GRCh37:</b> {{ record.MUTATION_LOCUS_IN_GRCh37 }}</p>
-              <p><b>MUTATION LOCUS IN GRCh38:</b> {{ record.MUTATION_LOCUS_IN_GRCh38 }}</p>
-              <p><b>MUTATION TYPE:</b> {{ record.MUTATION_TYPE }}</p>
-              <p><b>MUTATION CDS:</b> {{ record.MUTATION_CDS }}</p>
-              <p><b>GENOMIC REF ALLELE:</b> {{ record.GENOMIC_REF_ALLELE }}</p>
-              <p><b>GENOMIC MUT ALLELE:</b> {{ record.GENOMIC_MUT_ALLELE }}</p>
-              <p><b>DISEASE:</b> {{ record.DISEASE }}</p>
-            </div>
-          </template>
-        </s-table>
-      </s-table-provider>
+      <!-- 调整尺寸 -->
+      <div class="size-controls" style="margin-bottom: 10px">
+        <el-radio-group v-model="tableSize">
+          <el-radio-button value="small">Small Size</el-radio-button>
+          <el-radio-button value="default">Default Size</el-radio-button>
+          <el-radio-button value="large">Large Size</el-radio-button>
+        </el-radio-group>
+      </div>
     </div>
-  </template>
-  
-  <script lang="tsx">
-  import { defineComponent, ref, onMounted } from 'vue';
-  import { STableProvider } from '@shene/table';
-  import type { STableColumnsType } from '@shene/table';
-  import { useTableData } from '../../assets/js/useTableData.js';
-  
-  // 定义数据类型
-  type DataType = { [key: string]: string };
-  
-  export default defineComponent({
-    name: 'CodingVariationDisease2',
-    setup() {
-      const { searchText, filteredDataSource, loadData } = useTableData('/data/coding variation in Caner.csv');
-      const tableSize = ref('default'); // 表格尺寸状态
-  
-      onMounted(() => {
-        loadData();
+    <!-- 表格组件 -->
+    <s-table-provider :hover="true">
+      <s-table
+        :columns="columns"
+        :data-source="filteredDataSource"
+        :row-key="record => record.key"
+        :stripe="true"
+        :show-sorter-tooltip="true"
+        :size="tableSize"
+        :expand-row-by-click="true"
+      >
+        <template #bodyCell="{ text, column, record }">
+          <template v-if="column.key === 'name'">
+            <a>{{ text }}</a>
+          </template>
+          <template v-else-if="column.key === 'DISEASE'">
+            <ElSpace>
+              <ElTag v-for="items in (Array.isArray(record.DISEASE) ? record.DISEASE : record.DISEASE.split(';').map(str => str.trim()))" :key="items">
+                {{ items }}
+              </ElTag>
+            </ElSpace>
+          </template>
+        </template>
+        <template #expandedRowRender="{ record }">
+          <div>
+            <p><b>Gene Name:</b> {{ record.GENE_NAME }}</p>
+            <p><b>Ensembl ID:</b> {{ record.ENSEMBL_ID }}</p>
+            <p><b>Mutation URL:</b> <a :href="record.MUTATION_URL" target="_blank">{{ record.MUTATION_URL }}</a></p>
+            <p><b>Legacy Mutation ID:</b> {{ record.LEGACY_MUTATION_ID }}</p>
+            <p><b>Genomic Mutation ID:</b> {{ record.GENOMIC_MUTATION_ID }}</p>
+            <p><b>Mutation Locus in GRCh37:</b> {{ record.MUTATION_LOCUS_IN_GRCh37 }}</p>
+            <p><b>Mutation Locus in GRCh38:</b> {{ record.MUTATION_LOCUS_IN_GRCh38 }}</p>
+            <p><b>Mutation Type:</b> {{ record.MUTATION_TYPE }}</p>
+            <p><b>Mutation CDS:</b> {{ record.MUTATION_CDS }}</p>
+            <p><b>Genomic Ref Allele:</b> {{ record.GENOMIC_REF_ALLELE }}</p>
+            <p><b>Genomic Mut Allele:</b> {{ record.GENOMIC_MUT_ALLELE }}</p>
+            <p><b>Disease:</b>
+              <ElSpace>
+                <ElTag v-for="items in (Array.isArray(record.DISEASE) ? record.DISEASE : record.DISEASE.split(';').map(str => str.trim()))" :key="items">
+                  {{ items }}
+                </ElTag>
+              </ElSpace>
+            </p>
+          </div>
+        </template>
+      </s-table>
+    </s-table-provider>
+  </div>
+</template>
+
+<script lang="tsx">
+import { defineComponent, ref, onMounted } from 'vue';
+import { ElTooltip } from 'element-plus';
+import { STableProvider } from '@shene/table';
+import type { STableColumnsType } from '@shene/table';
+import { useTableData } from '../../assets/js/useTableData.js';
+
+// 定义数据类型
+type DataType = {
+  [key: string]: string | string[];
+  GENE_NAME: string;
+  ENSEMBL_ID: string;
+  MUTATION_URL: string;
+  LEGACY_MUTATION_ID: string;
+  GENOMIC_MUTATION_ID: string;
+  MUTATION_LOCUS_IN_GRCh37: string;
+  MUTATION_LOCUS_IN_GRCh38: string;
+  MUTATION_TYPE: string;
+  MUTATION_CDS: string;
+  GENOMIC_REF_ALLELE: string;
+  GENOMIC_MUT_ALLELE: string;
+  DISEASE: string[]; // 修改为数组类型
+};
+
+export default defineComponent({
+  name: 'CodingVariationDisease2',
+  components: {
+    ElTooltip
+  },
+  setup() {
+    const { searchText, filteredDataSource, loadData } = useTableData('/data/coding variation in Caner.csv', (data) => {
+      // 在加载数据时将 DISEASE 列转换为数组
+      return data.map(item => {
+        // 处理DISEASE字段
+        if (typeof item.DISEASE === 'string') {
+          item.DISEASE = item.DISEASE.split(';').map(str => str.trim());
+        }
+        return item;
       });
-  
-      const columns: STableColumnsType<DataType> = [
-        { title: 'GENE NAME', dataIndex: 'GENE_NAME', width: 150, ellipsis: true, key: 'GENE_NAME', resizable: true },
-        { title: 'ENSEMBL ID', dataIndex: 'ENSEMBL_ID', width: 150, ellipsis: true, key: 'ENSEMBL_ID', resizable: true },
-        { title: 'MUTATION URL', dataIndex: 'MUTATION_URL', width: 200, ellipsis: true, key: 'MUTATION_URL', resizable: true },
-        { title: 'LEGACY MUTATION ID', dataIndex: 'LEGACY_MUTATION_ID', width: 150, ellipsis: true, key: 'LEGACY_MUTATION_ID', resizable: true },
-        { title: 'GENOMIC MUTATION ID', dataIndex: 'GENOMIC_MUTATION_ID', width: 150, ellipsis: true, key: 'GENOMIC_MUTATION_ID', resizable: true },
-        { title: 'MUTATION LOCUS IN GRCh37', dataIndex: 'MUTATION_LOCUS_IN_GRCh37', width: 200, ellipsis: true, key: 'MUTATION_LOCUS_IN_GRCh37', resizable: true },
-        { title: 'MUTATION LOCUS IN GRCh38', dataIndex: 'MUTATION_LOCUS_IN_GRCh38', width: 200, ellipsis: true, key: 'MUTATION_LOCUS_IN_GRCh38', resizable: true },
-        { title: 'MUTATION TYPE', dataIndex: 'MUTATION_TYPE', width: 150, ellipsis: true, key: 'MUTATION_TYPE', resizable: true },
-        { title: 'MUTATION CDS', dataIndex: 'MUTATION_CDS', width: 150, ellipsis: true, key: 'MUTATION_CDS', resizable: true },
-        { title: 'GENOMIC REF ALLELE', dataIndex: 'GENOMIC_REF_ALLELE', width: 150, ellipsis: true, key: 'GENOMIC_REF_ALLELE', resizable: true },
-        { title: 'GENOMIC MUT ALLELE', dataIndex: 'GENOMIC_MUT_ALLELE', width: 150, ellipsis: true, key: 'GENOMIC_MUT_ALLELE', resizable: true },
-        { title: 'DISEASE', dataIndex: 'DISEASE', width: 200, ellipsis: true, key: 'DISEASE', resizable: true }
-      ];
-  
-      return {
-        columns,
-        filteredDataSource,
-        tableSize,
-        searchText,
-      };
-    }
-  });
-  </script>
-  
+    });
+
+    const tableSize = ref('default'); // 表格尺寸状态
+
+    onMounted(() => {
+      loadData();
+    });
+
+    const renderDiseaseTooltip = (disease) => {
+      // 确保 disease 是数组
+      const diseaseArray = Array.isArray(disease) ? disease : disease.split(';').map(str => str.trim());
+      return diseaseArray.map(item => item.split('/').join(' / ')).join('<br />');
+    };
+
+    const columns: STableColumnsType<DataType> = [
+      { title: 'Gene Name', dataIndex: 'GENE_NAME', width: 150, ellipsis: true, key: 'GENE_NAME', resizable: true },
+      { title: 'Ensembl ID', dataIndex: 'ENSEMBL_ID', width: 180, ellipsis: true, key: 'ENSEMBL_ID', resizable: true },
+      { title: 'Mutation URL', dataIndex: 'MUTATION_URL', width: 120, ellipsis: true, key: 'MUTATION_URL', customRender: ({ text, record }) => (<div><a href={text || '#'} target="_blank" class="bracket-links">Link</a></div>),
+        resizable: true },
+      { title: 'Legacy Mutation ID', dataIndex: 'LEGACY_MUTATION_ID', width: 150, ellipsis: true, key: 'LEGACY_MUTATION_ID', resizable: true },
+      { title: 'Genomic Mutation ID', dataIndex: 'GENOMIC_MUTATION_ID', width: 170, ellipsis: true, key: 'GENOMIC_MUTATION_ID', resizable: true },
+      { title: 'Mutation Locus in GRCh37', dataIndex: 'MUTATION_LOCUS_IN_GRCh37', width: 200, ellipsis: true, key: 'MUTATION_LOCUS_IN_GRCh37', resizable: true },
+      { title: 'Mutation Locus in GRCh38', dataIndex: 'MUTATION_LOCUS_IN_GRCh38', width: 200, ellipsis: true, key: 'MUTATION_LOCUS_IN_GRCh38', resizable: true },
+      { title: 'Mutation Type', dataIndex: 'MUTATION_TYPE', width: 150, ellipsis: true, key: 'MUTATION_TYPE', resizable: true },
+      { title: 'Mutation CDS', dataIndex: 'MUTATION_CDS', width: 150, ellipsis: true, key: 'MUTATION_CDS', resizable: true },
+      { title: 'Genomic Ref Allele', dataIndex: 'GENOMIC_REF_ALLELE', width: 150, ellipsis: true, key: 'GENOMIC_REF_ALLELE', resizable: true },
+      { title: 'Genomic Mut Allele', dataIndex: 'GENOMIC_MUT_ALLELE', width: 150, ellipsis: true, key: 'GENOMIC_MUT_ALLELE', resizable: true },
+      { 
+        title: 'Disease', 
+        dataIndex: 'DISEASE', 
+        width: 1200, 
+        ellipsis: true, 
+        key: 'DISEASE', 
+        resizable: true,
+      }
+    ];
+
+    return {
+      columns,
+      filteredDataSource,
+      tableSize,
+      searchText,
+      renderDiseaseTooltip
+    };
+  }
+});
+</script>
