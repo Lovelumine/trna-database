05db77eea4c65162cc5dc1a0b5d16ccc09df7785
Lovelumine <shendekoudai@lovelumine.com>
2024-05-23 19:52:03 +0800

    增加tRNAtherapeutics页面

---
+++ src/views/tRNAtherapeutics/tRNAtherapeutics.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="site--main">
+      <h2>Trna Therapeutics</h2>
+  
+      <div class="table-section">
+        <tRNAtherapeutics1 />
+      </div>
+  
+    </div>
+  </template>
+  
+  <script>
+  import tRNAtherapeutics1 from './tRNAtherapeutics-1.vue';
+  
+  export default {
+    name: 'tRNAtherapeutics',
+    components: {
+        tRNAtherapeutics1,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .table-section {
+    margin-bottom: 20px;
+  }
+  </style>
+  
