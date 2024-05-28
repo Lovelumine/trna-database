ee403c882e092547437b66cc2f6bd5b05ab5c96f
Lovelumine <shendekoudai@lovelumine.com>
2024-05-29 03:33:51 +0800

    修复聊天框拖动bug

--- src/bot/BotComponent.vue
+++ src/bot/BotComponent.vue
@@ -1,41 +1,39 @@
 <template>
-    <div id="bot-container" ref="botContainer" @mousedown="startDrag">
-      <div id="bot-icon" @click="toggleChat">
-        <img src="/bot-image.png" alt="Bot Icon" @dragstart.prevent />
-      </div>
-      <div id="chat-box" v-if="isChatOpen">
-        <div id="chat-header">
-          <span>Yingying</span>
-          <button @click="toggleChat">关闭</button>
-        </div>
-        <div id="chat-content">
-          <div v-for="message in messages" :key="message.id" :class="message.sender">
-            {{ message.text }}
-          </div>
-        </div>
-        <input
-          id="chat-input"
-          v-model="newMessage"
-          @keypress.enter="sendMessage"
-          placeholder="输入消息..."
-        />
-      </div>
+  <div id="bot-container" ref="element" @mousedown="startDrag">
+    <div id="bot-icon" @click="toggleChat">
+      <img src="/bot-image.png" alt="Bot Icon" @dragstart.prevent />
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { useDraggable } from './Draggable';
-  import { useChat } from './useChat';
-  
-  export default defineComponent({
-    name: 'BotComponent',
-    setup() {
-      const { element, startDrag } = useDraggable();
-      const { isChatOpen, messages, newMessage, toggleChat, sendMessage } = useChat();
-      return { element, startDrag, isChatOpen, messages, newMessage, toggleChat, sendMessage };
-    }
-  });
-  </script>
-  
-  
+    <div id="chat-box" v-if="isChatOpen">
+      <div id="chat-header">
+        <span>Yingying</span>
+        <button @click="toggleChat">Close</button>
+      </div>
+      <div id="chat-content">
+        <div v-for="message in messages" :key="message.id" :class="message.sender">
+          {{ message.text }}
+        </div>
+      </div>
+      <input
+        id="chat-input"
+        v-model="newMessage"
+        @keypress.enter="sendMessage"
+        placeholder="Type a message..."
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useDraggable } from './Draggable';
+import { useChat } from './useChat';
+
+export default defineComponent({
+  name: 'BotComponent',
+  setup() {
+    const { element, startDrag } = useDraggable();
+    const { isChatOpen, messages, newMessage, toggleChat, sendMessage } = useChat();
+    return { element, startDrag, isChatOpen, messages, newMessage, toggleChat, sendMessage };
+  }
+});
+</script>
