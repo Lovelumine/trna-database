312071b305222a9a577b96b55018d0cab9b5ca89
Lovelumine <shendekoudai@lovelumine.com>
2024-06-17 16:29:35 +0800

    增加细节

--- src/views/tRNA elements/tRNA-elements-1.vue
+++ src/views/tRNA elements/tRNA-elements-1.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="site--main">
-    <h2>tRNA Modify-function</h2>
+    <h2>tRNA Modification Functions</h2>
     <!-- 顶部行包含尺寸调整和搜索框 -->
     <div class="top-controls">
       <!-- 搜索框 -->
