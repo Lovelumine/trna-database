9e1564b3cd60f87bb6ef019973b8d7b0e3a8a006
Lovelumine <shendekoudai@lovelumine.com>
2024-06-20 13:47:40 +0800

    增加机器人预设

--- src/bot/presetInformation.ts
+++ src/bot/presetInformation.ts
@@ -16,3 +16,23 @@ export const projectContent = `Project Content:
 2. Natural sup-tRNA: What are the naturally occurring sup-tRNAs, in which species are they found? What are their sequences? What key positions have mutations?
 3. tRNA Therapeutics: For which diseases are the current tRNA therapies mainly used? What are their design strategies? What key sites have been modified? What amino acids have been introduced? Does the resulting protein structure change? How effective are they? How safe are they?
 4. tRNA Elements: What are the key elements on the tRNA molecule that determine its characteristics and functions?`;
+
+
+export const QA=`
+Q1：数据库该部分包含了什么内容？
+A1：包含了200余条tRNA修饰与其对应的功能的相关，包括功能实现的细胞，体现的表观特征以及对应的文献信息等。以及30余条tRNA的部分特征如特定位点的修饰对于修饰酶的功能，或者特定的碱基对于tRNA的生理功能实现的要求等。另外还收录了部分氨酰tRNA合成酶所需的底物tRNA的特定条件。
+
+Q2：数据库功能？
+A2：用户可以从数据库中搜索到tRNA特定修饰的特定功能，可以查看业内研究了那些修饰的功能，并且修饰的功能所实现的细胞，可以结合自身的研究，保留或删除某些修饰。另外在tRNA设计过程中，可以从本数据库中获得关键位点信息，提供了指导信息，确保设计的合理性及有效性。
+
+Q3：为什么一些修饰没有位点信息？
+A3：部分研究研究的是特定修饰的总体水平，所以没有特定tRNA信息，可以将其作为整体看待。
+
+Q4：如何搜索想要的信息？
+A4：用户可以直接使用搜索功能，但是请键入完整的信息。或者可以通过filter功能，将信息精简后通过自己处理获得信息。
+
+Q5：我可以拿这个数据库做什么？
+A5：可以将该部分作为一个指导信息，进行tRNA设计，或者作为一个训练集，将修饰的功能作为训练集信息，进行特定的训练。
+
+Q6：我只知道中文，不知道简写及符号是什么？
+A6：该数据库考虑了该部分，并且连接到了modomics（https://genesilico.pl/modomics/modifications）数据库，可以查看想要的修饰信息。`
