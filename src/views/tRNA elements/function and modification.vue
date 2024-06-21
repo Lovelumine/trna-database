0c14f0b873b8c71c183253a2ddf90a5526d3f3c4
Lovelumine <shendekoudai@lovelumine.com>
2024-06-21 13:05:50 +0800

    修改细节

--- src/views/tRNA elements/function and modification.vue
+++ src/views/tRNA elements/function and modification.vue
@@ -110,7 +110,8 @@ export default defineComponent({
       { title: 'Function of Modification', dataIndex: 'Function_of_Modification', width: 440, ellipsis: true, key: 'Function_of_Modification', resizable: true },
       { title: 'species', dataIndex: 'species', width: 200, ellipsis: true, key: 'species', resizable: true, customRender: ({ text, record }) => (<span className="latin-name">{record.species}</span>)},
       { title: 'tissue or cell line', dataIndex: 'tissue_or_cell_line', width: 240, ellipsis: true, key: 'tissue_or_cell_line', resizable: true },
-      { title: 'condition', dataIndex: 'condition', width: 200, ellipsis: true, key: 'condition', resizable: true },      {
+      { title: 'condition', dataIndex: 'condition', width: 200, ellipsis: true, key: 'condition', resizable: true },      
+      {
         title: 'PMID', width: 112, ellipsis: true, key: 'PMID', dataIndex: 'PMID',
         customRender: ({ text, record }) => (<div><a href={'https://pubmed.ncbi.nlm.nih.gov/' + record.PMID || '#'} target="_blank" class="bracket-links">{record.PMID}</a></div>),
         resizable: true
