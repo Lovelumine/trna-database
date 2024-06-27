4a1a91aeee50682c11a814a7b3f7701457655e2b
Lovelumine <shendekoudai@lovelumine.com>
2024-06-27 21:28:45 +0800

    修改tRNA elements

--- src/views/tRNA elements/tRNA elements.vue
+++ src/views/tRNA elements/tRNA elements.vue
@@ -5,18 +5,22 @@
       <div class="table-section">
         <trna-elements-1 />
       </div>
+
+      <div class="table-section">
+        <trna-elements-2 />
+      </div>
     </div>
   </template>
   
   <script>
   import TrnaElements1 from './function and modification.vue';
-
+  import TrnaElements2 from './aaRS recognition.vue';
   
   export default {
     name: 'TrnaElements',
     components: {
       TrnaElements1,
-
+      TrnaElements2
     },
   };
   </script>
