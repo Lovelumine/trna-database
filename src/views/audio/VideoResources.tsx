e06ae1336062960c3f3297acd53c1206c5f81453
lovelumine <shendekoudai@lovelumine.com>
2024-12-27 23:40:48 +0800

    修改audio页面

--- src/views/audio/VideoResources.tsx
+++ src/views/audio/VideoResources.tsx
@@ -1,9 +1,9 @@
 export const videoList = [
   {
     title: '双序列比对工具的介绍',
-    src: "/src/views/audio/audio/双序列比对工具的介绍.mp4",
+    src: "https://minio.lumoxuan.cn/ensure/video/%E5%8F%8C%E5%BA%8F%E5%88%97%E6%AF%94%E5%AF%B9%E5%B7%A5%E5%85%B7%E7%9A%84%E4%BB%8B%E7%BB%8D.mp4",
     poster: "/src/views/audio/audio/双序列比对工具的介绍.png",
-    subtitles: "/src/views/audio/audio/双序列比对工具的介绍.srt",
+    subtitles: "https://minio.lumoxuan.cn/ensure/video/%E5%8F%8C%E5%BA%8F%E5%88%97%E6%AF%94%E5%AF%B9%E5%B7%A5%E5%85%B7%E7%9A%84%E4%BB%8B%E7%BB%8D.srt",
   },
   // {
   //   title: '起始、延伸与终止-原核生物',
